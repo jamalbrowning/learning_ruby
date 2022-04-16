84d6c848a1e7f320826920fc236cbb2e39c35813
Jim Browning <jamalebrowning@gmail.com>
2022-04-15 22:04:50 -0500

    finished exercise 4

--- ex4.rb
+++ ex4.rb
@@ -1,4 +1,24 @@
+#assignment for cars
 cars = 100
+#assignment for space in a car
 space_in_a_car = 4.0
+#assignment for drivers
 drivers = 30
+#assignment for passengers
 passengers = 90
+#assignment for cars not driven
+cars_not_driven = cars - drivers
+#assignment for cars driven
+cars_driven = drivers
+#assignment for carpool capacitu
+carpool_capacity = cars_driven * space_in_a_car
+#assignment for the average amount of people per car
+average_passengers_per_car = passengers / cars_driven
+
+
+puts "There are #{cars} cars available."
+puts "There are only #{drivers} drivers available."
+puts "There will be #{cars_not_driven} empty cars today."
+puts "We can transport #{carpool_capacity} people today."
+puts "We have #{passengers} to carpool today."
+puts "We need to put about #{average_passengers_per_car} in each car."
