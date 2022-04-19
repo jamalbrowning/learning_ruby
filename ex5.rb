ec5be37005c715e8a4be2549ca95b9b616528ffa
Jim Browning <jamalebrowning@gmail.com>
2022-04-19 14:44:28 -0500

    ex5

---
+++ ex5.rb
@@ -0,0 +1,15 @@
+name = 'Zed A. Shaw'
+age = 31
+height = 65
+weight = 199
+eyes = 'Brown'
+teeth = 'White'
+hair = 'Brown'
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall."
+puts "He's #{weight} popunds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee"
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
